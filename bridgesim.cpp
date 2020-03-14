e5962ee789ffbd737f1e746ccf8abc225d047bca
Sumanyu Ghoshal <sumanyu.ghoshal@gmail.com>
2020-03-14 07:04:28 +0530

    Created empty bridgesim.cpp

---
+++ bridgesim.cpp
@@ -0,0 +1,11 @@
+#include "bridge.cpp"
+#include <vector>
+#include <map>
+using namespace std;
+class Network
+{
+    vector<Bridge> bridges;
+    map<char, int> lan_number_map;
+    li lan_entries;
+
+};
