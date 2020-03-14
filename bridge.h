d84d5aee27a9733bbce7168f2a2153412c55ba07
Sumanyu Ghoshal <sumanyu.ghoshal@gmail.com>
2020-03-14 16:47:08 +0530

    Edited the bridge with appropriate changes.

--- bridge.h
+++ bridge.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <map>
+#include <utility>
 typedef std::vector<char> li;
 typedef std::vector<int> state;
 struct Message
@@ -12,18 +14,17 @@ public:
 	Message operator=(const Message &rhs);
 	Message();
 	Message(int rp_in, int d_in, int sp_in);
-	Message increment_distance();
+	Message increment_distance(int newpos);
 
 }; // A message has been defined.
-typedef std::vector<Message> received_messages;
+typedef std::vector<std::pair<char, Message>> received_messages;
 
 class Bridge
 {
 public:
 	int pos;
-	li fields;
-	state st;
 	received_messages rm;
+	std::map<char, int> lan_status;
 
 	//
 	// s-> 0 --- Designated Port
@@ -37,4 +38,27 @@ public:
 	void update_state(); // this will update the output configuration messages that are to be sent.
 	li send_config_message();
 	Message out_config_mes();
+	void output();
 }; //A bridge with basic functionalities defined.
+
+class Network
+{
+	int t;
+	int n_bridges;
+	// string inp;
+
+	std::vector<Bridge> bridges;		//this will store all the bridges.
+	std::map<char, int> lan_number_map; //Create a numbering between lan connection and the number it is assigned.
+	std::vector<bool> config_has_b1;
+	li int_to_lan;
+	std::vector<std::vector<int>> lan_to_bridge;
+
+public:
+	Network();
+	void input_t();
+	void input_bridges();
+	void process_bridges();
+	void output_bridges();
+	// void input_lan();
+	// void process_lan();
+};
